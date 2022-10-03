098c5a18bddebce3f069875b3a97302759e27d58
hmbemba <hmbemba@gmail.com>
2022-10-02 22:06:39 -0400

    arg

--- spotipy_stuff/Spotipy_API_Config.py
+++ spotipy_stuff/Spotipy_API_Config.py
@@ -2,19 +2,10 @@ from os import environ
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
-<<<<<<< HEAD
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-
-
-=======
 import os
 from dotenv import load_dotenv
 
 load_dotenv() 
->>>>>>> 947e9df3c0ab06b2d9b2a8c7e511058eddb0d61a
 
 auth_manager = SpotifyClientCredentials(
   client_id = os.environ['SPOTIPY_CLIENT_ID'],
