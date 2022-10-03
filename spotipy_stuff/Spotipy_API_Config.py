0e34e6ab8a46a63013843b92eaad223c21dcb928
hmbemba <hmbemba@gmail.com>
2022-10-02 21:45:34 -0400

    old

--- spotipy_stuff/Spotipy_API_Config.py
+++ spotipy_stuff/Spotipy_API_Config.py
@@ -1,14 +1,16 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-SPOTIPY_CLIENT_ID='65733477d5d145e990d262ea7854c537'
-SPOTIPY_CLIENT_SECRET='739153f14e2940148da0340819b59512'
+
 
 
 auth_manager = SpotifyClientCredentials(
-  client_id = SPOTIPY_CLIENT_ID,
-  client_secret = SPOTIPY_CLIENT_SECRET
+  client_id = os.environ['SPOTIPY_CLIENT_ID'],
+  client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
 )
 spotipy_client = spotipy.Spotify(auth_manager=auth_manager)
 
